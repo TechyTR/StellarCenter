7a45353dde0154bc1e05b57ae47bfcf76fcab01d
TechyTR <hasanakmercan05@gmail.com>
2026-09-07 00:24:01 +0300

    Update StellarNavigation.kt

--- android/app/src/main/kotlin/org/test/thislinux/ui/navigation/StellarNavigation.kt
+++ android/app/src/main/kotlin/org/test/thislinux/ui/navigation/StellarNavigation.kt
@@ -1,21 +1,14 @@
 package org.test.thislinux.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.material3.Text
 
 @Composable
 fun StellarNavigation(
     selectedIndex: Int
 ) {
-
     when (selectedIndex) {
-
-        0 -> HomeScreen()
-
-        1 -> SystemMonitorScreen()
-
-        2 -> StorageScreen()
-
-        3 -> SettingsScreen()
+        0 -> SystemMonitorScreen()
+        1 -> NotesScreen()
+        2 -> AppScreen()
     }
 }
