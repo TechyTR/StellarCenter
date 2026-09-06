a0f5ba0de6302ea1e189f33ff77ebeced3614cc9
TechyTR <hasanakmercan05@gmail.com>
2026-09-07 01:47:38 +0300

    Update BottomNavBar.kt

--- android/app/src/main/kotlin/org/test/thislinux/BottomNavBar.kt
+++ android/app/src/main/kotlin/org/test/thislinux/BottomNavBar.kt
@@ -17,12 +17,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Dashboard
 import androidx.compose.material.icons.filled.Info
@@ -42,6 +42,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -303,8 +304,7 @@ fun BottomNavBar(
                                     ),
                                 size =
                                     size.copy(
-                                        height =
-                                            size.height
+                                        height = size.height
                                     )
                             )
 
@@ -329,8 +329,7 @@ fun BottomNavBar(
                                     ),
                                 size =
                                     size.copy(
-                                        height =
-                                            size.height
+                                        height = size.height
                                     )
                             )
                         }
