614447a563723869574767e8955b2595e8c57d86
TechyTR <hasanakmercan05@gmail.com>
2026-09-07 00:23:31 +0300

    Update StellarCenterApp.kt

--- android/app/src/main/kotlin/org/test/thislinux/ui/StellarCenterApp.kt
+++ android/app/src/main/kotlin/org/test/thislinux/ui/StellarCenterApp.kt
@@ -4,16 +4,26 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.test.thislinux.ui.navigation.StellarNavigation
+import org.test.thislinux.ui.theme.StellarAccent
 import org.test.thislinux.ui.theme.StellarTheme
+import org.test.thislinux.ui.theme.StellarThemeStyle
 
 @Composable
 fun StellarCenterApp() {
+    val themeStyle = StellarThemeStyle.LIQUID_GLASS_LIGHT
+    val accent = StellarAccent.PURPLE
 
-    StellarTheme {
-
+    StellarTheme(
+        style = themeStyle,
+        accent = accent
+    ) {
         var selectedIndex by remember {
             mutableIntStateOf(0)
         }
@@ -23,19 +33,18 @@ fun StellarCenterApp() {
             bottomBar = {
                 StellarBottomBar(
                     selectedIndex = selectedIndex,
+                    themeStyle = themeStyle,
                     onSelected = {
                         selectedIndex = it
                     }
                 )
             }
         ) { padding ->
-
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(padding)
             ) {
-
                 StellarNavigation(
                     selectedIndex = selectedIndex
                 )
