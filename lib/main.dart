c468f3a7145e3c283ec00c6bedd2ce598970a116
TechyTR <hasanakmercan05@gmail.com>
2026-09-09 14:28:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'theme/app_theme.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const stellarcenterapp());
+  runApp(const thislinuxapp());
 }
 
 class ThisLinuxApp extends StatefulWidget {
