fcd1e0b22a9fe2e79ddf14429dd428a3b4785e49
TechyTR <hasanakmercan05@gmail.com>
2026-09-24 19:32:13 +0300

    Create repeat_mode.dart

---
+++ lib/Linux/music/repeat_mode.dart
@@ -0,0 +1,21 @@
+enum StellarRepeatMode {
+  playlistOnce,
+  songForever,
+  albumOnce,
+  albumForever,
+}
+
+extension StellarRepeatModeX on StellarRepeatMode {
+  String get label {
+    switch (this) {
+      case StellarRepeatMode.playlistOnce:
+        return '1×';
+      case StellarRepeatMode.songForever:
+        return '∞';
+      case StellarRepeatMode.albumOnce:
+        return 'ALB 1×';
+      case StellarRepeatMode.albumForever:
+        return 'ALB ∞';
+    }
+  }
+}
