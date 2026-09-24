a804993e89a5d5cf10dbc858052bf8dbee7556c9
TechyTR <hasanakmercan05@gmail.com>
2026-09-24 21:31:49 +0300

    Update main_android.dart

--- lib/main_android.dart
+++ lib/main_android.dart
@@ -1,118 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'pages/boot_screen.dart';
-import 'services/preferences_service.dart';
-import 'theme/app_theme.dart';
+import 'app.dart';
+import 'platform/android/home_shell_android.dart';
 
-typedef HomeShellBuilder = Widget Function({
-  required AppThemeColor selectedTheme,
-  required AppThemeStyle selectedStyle,
-  required Future<void> Function(AppThemeColor) onThemeChanged,
-  required Future<void> Function(AppThemeStyle) onStyleChanged,
-});
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class StellarCenterApp extends StatefulWidget {
-  final HomeShellBuilder homeShellBuilder;
+  await Firebase.initializeApp();
 
-  const StellarCenterApp({
-    super.key,
-    required this.homeShellBuilder,
-  });
-
-  @override
-  State<StellarCenterApp> createState() =>
-      _StellarCenterAppState();
-}
-
-class _StellarCenterAppState
-    extends State<StellarCenterApp> {
-  AppThemeColor _selectedTheme =
-      AppThemeColor.purple;
-
-  AppThemeStyle _selectedStyle =
-      AppThemeStyle.normal;
-
-  bool _preferencesLoaded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadPreferences();
-  }
-
-  Future<void> _loadPreferences() async {
-    final colorValue =
-        await PreferencesService.getThemeColor();
-
-    final styleValue =
-        await PreferencesService.getThemeStyle();
-
-    if (!mounted) return;
-
-    setState(() {
-      _selectedTheme =
-          AppTheme.colorFromString(colorValue);
-
-      _selectedStyle =
-          AppTheme.styleFromString(styleValue);
-
-      _preferencesLoaded = true;
-    });
-  }
-
-  Future<void> _changeTheme(
-    AppThemeColor color,
-  ) async {
-    setState(() {
-      _selectedTheme = color;
-    });
-
-    await PreferencesService.saveThemeColor(
-      AppTheme.colorToString(color),
-    );
-  }
-
-  Future<void> _changeStyle(
-    AppThemeStyle style,
-  ) async {
-    setState(() {
-      _selectedStyle = style;
-    });
-
-    await PreferencesService.saveThemeStyle(
-      AppTheme.styleToString(style),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (!_preferencesLoaded) {
-      return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.black,
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        ),
-      );
-    }
-
-    return MaterialApp(
-      title: 'Stellar Center',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.build(
-        _selectedTheme,
-        _selectedStyle,
-      ),
-      home: BootScreen(
-        selectedTheme: _selectedTheme,
-        selectedStyle: _selectedStyle,
-        onThemeChanged: _changeTheme,
-        onStyleChanged: _changeStyle,
-        homeShellBuilder:
-            widget.homeShellBuilder,
-      ),
-    );
-  }
+  runApp(
+    StellarCenterApp(
+      homeShellBuilder: ({
+        required selectedTheme,
+        required selectedStyle,
+        required onThemeChanged,
+        required onStyleChanged,
+      }) {
+        return HomeShellAndroid(
+          selectedTheme: selectedTheme,
+          selectedStyle: selectedStyle,
+          onThemeChanged: onThemeChanged,
+          onStyleChanged: onStyleChanged,
+        );
+      },
+    ),
+  );
 }
