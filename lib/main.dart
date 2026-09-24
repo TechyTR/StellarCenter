c68441af7609ca103462910aa44e048e93ae58ff
TechyTR <hasanakmercan05@gmail.com>
2026-09-24 21:17:23 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'main_android.dart'
 import 'pages/boot_screen.dart';
 import 'services/preferences_service.dart';
 import 'theme/app_theme.dart';
