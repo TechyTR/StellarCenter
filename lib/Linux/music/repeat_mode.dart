a9310787a0152c6ddc9852c2d7c015ecd9601e49
TechyTR <hasanakmercan05@gmail.com>
2026-09-24 19:47:59 +0300

    Update repeat_mode.dart

--- lib/Linux/music/repeat_mode.dart
+++ lib/Linux/music/repeat_mode.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 enum StellarRepeatMode {
   playlistOnce,
   songForever,
@@ -10,12 +12,31 @@ extension StellarRepeatModeX on StellarRepeatMode {
     switch (this) {
       case StellarRepeatMode.playlistOnce:
         return '1×';
+
       case StellarRepeatMode.songForever:
         return '∞';
+
       case StellarRepeatMode.albumOnce:
         return 'ALB 1×';
+
       case StellarRepeatMode.albumForever:
         return 'ALB ∞';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case StellarRepeatMode.playlistOnce:
+        return Icons.repeat;
+
+      case StellarRepeatMode.songForever:
+        return Icons.repeat_one;
+
+      case StellarRepeatMode.albumOnce:
+        return Icons.album;
+
+      case StellarRepeatMode.albumForever:
+        return Icons.album_outlined;
     }
   }
 }
