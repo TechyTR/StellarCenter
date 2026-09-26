a9f4b031ba18282a42d4d7960a02fea9c9b3f3e3
TechyTR <hasanakmercan05@gmail.com>
2026-09-26 17:34:23 +0300

    Update linux_navigation_bar.dart

--- lib/Linux/widgets/linux_navigation_bar.dart
+++ lib/Linux/widgets/linux_navigation_bar.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-import '../../theme/app_theme.dart';
+import '../../both/theme/app_theme.dart';
 
 class LinuxNavigationBar extends StatelessWidget {
   final int currentIndex;
