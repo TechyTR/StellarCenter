706b5a80bbfeba5e0c7eacae1c96da6582ca3fd3
TechyTR <hasanakmercan05@gmail.com>
2026-09-26 16:55:38 +0300

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -10,7 +10,7 @@ class AuthService {
   static final AuthService instance = AuthService._();
 
   static const String _firebaseApiKey =
-      'BURAYA_FIREBASE_WEB_API_KEY';
+      'FIREBASE_APIKEY';
 
   static const String _baseUrl =
       'https://identitytoolkit.googleapis.com/v1/accounts';
