989eb3cc1b53c321e85c876330da14ea6dae95ed
TechyTR <hasanakmercan05@gmail.com>
2026-09-26 17:31:05 +0300

    Update main_android.dart

--- lib/main/main_android.dart
+++ lib/main/main_android.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
-import '../platform/android/home_shell_android.dart';
+import '../pages/home_shell_android.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
